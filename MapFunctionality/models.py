d8fe9dd0118b33977377909af2f9307861f20ec5
Jordan Rose <jordanrose@dal.ca>
2018-09-15 01:50:03 -0400

    Added static map

--- MapFunctionality/models.py
+++ MapFunctionality/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Meetup(models.Model):
+    Latitude = models.FloatField()
+    Longitude = models.FloatField()
+    StartTime = models.TimeField()
+    EndTime = models.TimeField()
+    Date = models.DateField()
+    Name = models.CharField(max_length=50)
